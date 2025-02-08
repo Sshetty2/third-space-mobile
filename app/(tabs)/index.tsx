894e29b7243454a83eb49438aaf98279834200a7
Sachit Shetty <sachit.shetty@gmail.com>
2025-02-08 18:32:50 -0500

    add gluestack

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,17 @@
-import { StyleSheet } from 'react-native';
-
+import { Box } from "@/components/ui/box"
+import { Text } from "@/components/ui/text"
+import { Divider } from "@/components/ui/divider"
+import { VStack } from "@/components/ui/vstack"
 import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <Box className="flex-1 items-center justify-center">
+      <VStack space="md">
+        <Text className="text-xl font-bold text-center">Tab One</Text>
+        <Divider className="my-6 w-4/5" />
+        <EditScreenInfo path="app/(tabs)/index.tsx" />
+      </VStack>
+    </Box>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
